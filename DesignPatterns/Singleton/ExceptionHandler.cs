2d2a71dbe39a8ca66dcddc683ed118b228835d22
shamim hossain <shshamim@aci-bd.com>
2018-05-13 11:58:34 +0600

    singleton

---
+++ DesignPatterns/Singleton/ExceptionHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Singleton
+{
+    public sealed class ExceptionHandler
+    {
+
+        private static readonly Lazy<ExceptionHandler>
+            Instance = new Lazy<ExceptionHandler>(() => new ExceptionHandler());
+
+        private static int _counter;
+        private ExceptionHandler()
+        {
+            _counter++;
+            Console.WriteLine("Object counter: " + _counter);
+        }
+
+        public static ExceptionHandler GetInstance => Instance.Value;
+
+        public void LogException(Exception exception)
+        {
+            Console.WriteLine("Exception: " + exception.Message + " at " + DateTime.Now);
+        }
+    }
+}
