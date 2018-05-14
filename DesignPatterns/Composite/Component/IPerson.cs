6f4f22919ef5d6eb4c45226ab66a3dc77c16413c
shamim hossain <shshamim@aci-bd.com>
2018-05-14 13:04:34 +0600

    composite

---
+++ DesignPatterns/Composite/Component/IPerson.cs
@@ -0,0 +1,8 @@
+﻿namespace Composite.Component
+{
+    public interface IPerson
+    {
+        int Amount { get; set; }
+        void PrintStatus();
+    }
+}
---
+++ DesignPatterns/Composite/Leaf/Person.cs
@@ -0,0 +1,15 @@
+﻿using Composite.Component;
+using System;
+
+namespace Composite.Leaf
+{
+    public class Person : IPerson
+    {
+        public string Name { get; set; }
+        public int Amount { get; set; }
+        public void PrintStatus()
+        {
+            Console.WriteLine($"{Name} has {Amount} Dollars");
+        }
+    }
+}
