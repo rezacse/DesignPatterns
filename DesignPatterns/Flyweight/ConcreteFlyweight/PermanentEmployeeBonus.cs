bb55ffd3e383d91b37b1eb0c1c7c6f0e6ab0a948
shamim hossain <shshamim@aci-bd.com>
2018-05-15 12:21:40 +0600

    Flyweight

---
+++ DesignPatterns/Flyweight/ConcreteFlyweight/PermanentEmployeeBonus.cs
@@ -0,0 +1,13 @@
+﻿using Flyweight.Flyweight;
+
+namespace Flyweight.ConcreteFlyweight
+{
+    public class PermanentEmployeeBonus : IEmployeeBonus
+    {
+        public double CalculateBonus(double salary)
+        {
+            //50% of bonus
+            return salary * 0.50;
+        }
+    }
+}
---
+++ DesignPatterns/Flyweight/FlyweightFactory/EmployeeBonusFactory.cs
@@ -0,0 +1,44 @@
+﻿using Flyweight.ConcreteFlyweight;
+using Flyweight.Flyweight;
+using System;
+using System.Collections.Generic;
+
+namespace Flyweight.FlyweightFactory
+{
+    public sealed class EmployeeBonusFactory
+    {
+
+
+        #region singleton
+        private static readonly Lazy<EmployeeBonusFactory> Instance = new Lazy<EmployeeBonusFactory>(() => new EmployeeBonusFactory());
+        private EmployeeBonusFactory()
+        {
+
+        }
+
+        public static EmployeeBonusFactory GetInstance => Instance.Value;
+        #endregion
+
+        private readonly Dictionary<string, IEmployeeBonus> _employeeBonusObjects = new Dictionary<string, IEmployeeBonus>();
+        public int TotalObject => _employeeBonusObjects.Count;
+        public IEmployeeBonus GetEmployeeBonusObject(string employeeType)
+        {
+            if (_employeeBonusObjects.ContainsKey(employeeType))
+                return _employeeBonusObjects[employeeType];
+
+            switch (employeeType)
+            {
+                case "Permanent":
+                    _employeeBonusObjects.Add(employeeType, new PermanentEmployeeBonus());
+                    break;
+                case "Contract":
+                    _employeeBonusObjects.Add(employeeType, new ContractEmployeeBonus());
+                    break;
+                default:
+                    throw new ArgumentException("Unknown employee type");
+            }
+
+            return _employeeBonusObjects[employeeType];
+        }
+    }
+}
