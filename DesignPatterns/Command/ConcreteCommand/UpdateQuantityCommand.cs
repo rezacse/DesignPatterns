eac334e4ab0e871cf48aa5014244bd8bd2560163
shamim hossain <shshamim@aci-bd.com>
2018-05-15 17:56:10 +0600

    Command

---
+++ DesignPatterns/Command/ConcreteCommand/UpdateQuantityCommand.cs
@@ -0,0 +1,27 @@
+﻿using Command.Command;
+using System;
+
+namespace Command.ConcreteCommand
+{
+    public class UpdateQuantityCommand : ICustomCommand, ICommandFactory
+    {
+        public int NewQuantity { get; set; }
+        public void Execute()
+        {
+            //fetch from database or other source
+            const int oldQuantity = 5;
+            Console.WriteLine("Update Database");
+
+            //log the update by who
+            Console.WriteLine($"Log: update quantity from {oldQuantity} to {NewQuantity}");
+        }
+
+        public string CommandName => "UpdateQuantity";
+        public string Description => "UpdateQuantity Number";
+
+        public ICustomCommand MakeCommand(string value)
+        {
+            return new UpdateQuantityCommand { NewQuantity = int.Parse(value) };
+        }
+    }
+}
---
+++ DesignPatterns/Command/Program.cs
@@ -0,0 +1,23 @@
+﻿using Command.Client;
+using System;
+
+namespace Command
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var commandHandler = new CommandHandler();
+
+            args = new[] { "UpdateQuantity", "20" };
+            commandHandler.Run(args);
+
+
+            args = new[] { "NotMatch", "20" };
+            commandHandler.Run(args);
+
+
+            Console.ReadKey();
+        }
+    }
+}
