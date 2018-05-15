efed3c0e8a86fa3a27d3911367e46227bbabf168
shamim hossain <shshamim@aci-bd.com>
2018-05-15 15:36:58 +0600

    Builder

---
+++ DesignPatterns/Builder/Builder/CarBuilder.cs
@@ -0,0 +1,22 @@
+﻿using Builder.Product;
+
+namespace Builder.Builder
+{
+    public abstract class CarBuilder
+    {
+        protected Car Car;
+        public void CreateNewCar()
+        {
+            Car = new Car();
+        }
+
+        public Car GetCar()
+        {
+            return Car;
+        }
+
+        public abstract void SetTopSpeed();
+        public abstract void SetHorsePower();
+        public abstract void SetImpressiveFeature();
+    }
+}
---
+++ DesignPatterns/Builder/Product/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Builder.Product
+{
+    public class Car
+    {
+        public int TopSpeed { get; set; }
+        public double HorsePower { get; set; }
+        public string ImpressiveFeature { get; set; }
+
+
+        public void DisplayCarInfo()
+        {
+            Console.WriteLine("Here is requested Car information" +
+                              "\n--------------------------------------" +
+                              $"\nTop Speed: {TopSpeed}" +
+                              $"\nHorse Power: {HorsePower}" +
+                              $"\nImpressive Feature: {ImpressiveFeature}\n");
+        }
+    }
+}
