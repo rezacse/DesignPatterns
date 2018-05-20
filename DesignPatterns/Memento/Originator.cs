6b6b62501b43dbc7a0fedb9a323792579712a409
shamim hossain <shshamim@aci-bd.com>
2018-05-20 10:57:50 +0600

    Memento

---
+++ DesignPatterns/Memento/Originator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Memento
+{
+    public class Originator
+    {
+        private string _state;
+
+        public void SetState(string state)
+        {
+            Console.WriteLine("------------------");
+            Console.WriteLine($"Set State: state = {state}");
+            _state = state;
+        }
+
+        public Memento CreateMemento()
+        {
+            Console.WriteLine($"Create Memento: state = {_state}");
+            return new Memento(_state);
+        }
+
+        public void RestoreMemento(Memento memento)
+        {
+            _state = memento.GetState;
+            Console.WriteLine($"Restore Memento: state = {_state}");
+        }
+
+    }
+}
